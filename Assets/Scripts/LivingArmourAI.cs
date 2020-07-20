d0d3f842dd35d7b7f459c442a00c1f413755a5a7
Sean <seanclading@gmail.com>
2020-07-20 16:01:42 +1000

    AI and environment
    added environment that riley made and added a wuick ai that can patrol around the environment

---
+++ Assets/Scripts/LivingArmourAI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
+
+public class LivingArmourAI : MonoBehaviour
+{
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame  
+    void Update()
+    {
+        if(!agent.hasPath)
+        {
+            agent.SetDestination(RandomNavSphere(agent.GetComponent<Transform>().position, 50f, -1));
+        }
+        
+        
+    }
+    public static Vector3 RandomNavSphere(Vector3 origin, float distance, int layermask)
+    {
+        Vector3 randomDirection = UnityEngine.Random.insideUnitSphere * distance;
+
+        randomDirection += origin;
+
+        NavMeshHit navHit;
+
+        NavMesh.SamplePosition(randomDirection, out navHit, distance, layermask);
+
+        return navHit.position;
+    }
+}
