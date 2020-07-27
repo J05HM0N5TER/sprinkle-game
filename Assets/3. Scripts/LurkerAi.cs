69f654eb4fe9c5e893f0df29eb4a253836a1984b
Sean <seanclading@gmail.com>
2020-07-27 16:06:35 +1000

    fixed error with player controller and added lurkerai script WIP

---
+++ Assets/3. Scripts/LurkerAi.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LurkerAi : MonoBehaviour
+{
+    //public List<GameObject> lurkerPoints = new List<GameObject>();
+    public GameObject[] lurkerPoints;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lurkerPoints = GameObject.FindGameObjectsWithTag("Respawn");
+        foreach (GameObject Lurkerpoint in lurkerPoints)
+        {
+            print(Lurkerpoint.transform.position);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if looked at disable / invis
+        if (gameObject.GetComponent<Renderer>().isVisible)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            gameObject.SetActive(true);
+        }
+    }
+}
