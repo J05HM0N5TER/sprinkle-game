2749ef12b89b1af50026403157d27a14bd4798d4
Sean <seanclading@gmail.com>
2020-07-28 15:47:11 +1000

    mostly got Lurker working, some bugs, will bug test to fix (WIP)

--- Assets/3. Scripts/LurkerAi.cs
+++ Assets/3. Scripts/LurkerAi.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 public class LurkerAi : MonoBehaviour
@@ -7,6 +8,11 @@ public class LurkerAi : MonoBehaviour
     //public List<GameObject> lurkerPoints = new List<GameObject>();
     public GameObject[] lurkerPoints;
     public GameObject player;
+    GameObject currentLurkingPoint;
+    public bool wasSeen;
+    public GameObject closestLurkerPoint;
+    public float closestDistance;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,11 +29,47 @@ public class LurkerAi : MonoBehaviour
         // if looked at disable / invis
         if (gameObject.GetComponent<Renderer>().isVisible)
         {
-            gameObject.SetActive(false);
+            //gameObject.SetActive(false);
+            wasSeen = true;
         }
         else
         {
-            gameObject.SetActive(true);
+            //gameObject.SetActive(true);
         }
+        if(wasSeen)
+        {
+            foreach (GameObject Lurkerpoint in lurkerPoints)
+            {
+                bool availableLurkPoint;
+                if (Lurkerpoint.GetComponent<LurkerPoint>().isVisable)
+                {
+                    availableLurkPoint = false;
+                }
+                if (!Lurkerpoint.GetComponent<LurkerPoint>().isVisable)
+                {
+                    availableLurkPoint = true;
+                    float distance = UnityEngine.Vector3.Distance(player.GetComponent<Transform>().position, Lurkerpoint.GetComponent<Transform>().position);
+                    if(closestLurkerPoint == null)
+                    {
+                        closestLurkerPoint = Lurkerpoint;
+                        closestDistance = distance;
+                    }
+                    if(distance < closestDistance)
+                    {
+                        closestLurkerPoint = Lurkerpoint;
+                        closestDistance = distance;
+                    }
+                    
+                        //Physics.Linecast(player.GetComponent<Transform>().position, Lurkerpoint.transform.position)
+                }
+                
+            }
+            gameObject.GetComponent<Transform>().position = closestLurkerPoint.transform.position;
+            wasSeen = false;
+        }
+        
+
+
+
     }
 }
---
+++ Assets/3. Scripts/LurkerPoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LurkerPoint : MonoBehaviour
+{
+    public bool isVisable = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.GetComponent<Renderer>().isVisible)
+        {
+            isVisable = true;
+        }
+        else
+        {
+            isVisable = false;
+        }
+    }
+}
