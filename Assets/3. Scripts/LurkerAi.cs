e2a5e981fbf997c524018cd42c4bb2780aee9c2f
Sean <seanclading@gmail.com>
2020-07-29 11:25:09 +1000

    debugging of lurker

--- Assets/3. Scripts/LurkerAi.cs
+++ Assets/3. Scripts/LurkerAi.cs
@@ -9,10 +9,11 @@ public class LurkerAi : MonoBehaviour
     public GameObject[] lurkerPoints;
     public GameObject player;
     GameObject currentLurkingPoint;
-    public bool wasSeen;
-    public GameObject closestLurkerPoint;
-    public float closestDistance;
-    public bool debugIsVisable;
+    private GameObject closestLurkerPoint;
+    private float closestDistance;
+    private bool debugIsVisable;
+
+    public Camera playerCam;
 
     // Start is called before the first frame update
     void Start()
@@ -20,40 +21,24 @@ public class LurkerAi : MonoBehaviour
         lurkerPoints = GameObject.FindGameObjectsWithTag("Respawn");
         foreach (GameObject Lurkerpoint in lurkerPoints)
         {
-            print(Lurkerpoint.transform.position);
+            print("Lurker point: " + Lurkerpoint.transform.position);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        // if looked at disable / invis
-        if (gameObject.GetComponent<Renderer>().isVisible)
+        // Is the lurker being seen?
+        if (IsVisableToPlayer(transform.position))// gameObject.GetComponent<Renderer>().isVisible)
         {
-            //gameObject.SetActive(false);
-            gameObject.GetComponent<Renderer>().material.color = Color.green;
             debugIsVisable = true;
-            wasSeen = true;
-        }
-        if(!gameObject.GetComponent<Renderer>().isVisible)
-        {
-            //gameObject.SetActive(true);
-            gameObject.GetComponent<Renderer>().material.color = Color.red;
-            debugIsVisable = false;
-        }
-        if(wasSeen)
-        {
+            print("Is seen, trying to find new point...");
             foreach (GameObject Lurkerpoint in lurkerPoints)
             {
-                bool availableLurkPoint;
-                if (Lurkerpoint.GetComponent<LurkerPoint>().isVisableByPlayer)
+                // Is the spawn point being seen?
+                if (IsVisableToPlayer(Lurkerpoint.transform.position))
                 {
-                    availableLurkPoint = false;
-                    
-                }
-                if (!Lurkerpoint.GetComponent<LurkerPoint>().isVisableByPlayer)
-                {
-                    availableLurkPoint = true;
+                    // Find closest valid spawn point
                     float distance = UnityEngine.Vector3.Distance(player.GetComponent<Transform>().position, Lurkerpoint.GetComponent<Transform>().position);
                     if (closestLurkerPoint == null)
                     {
@@ -65,17 +50,42 @@ public class LurkerAi : MonoBehaviour
                         closestLurkerPoint = Lurkerpoint;
                         closestDistance = distance;
                     }
+                    
 
                     //Physics.Linecast(player.GetComponent<Transform>().position, Lurkerpoint.transform.position)
                 }
-                
+
             }
-            gameObject.GetComponent<Transform>().position = closestLurkerPoint.transform.position;
-            wasSeen = false;
+            if (closestLurkerPoint)
+            {
+                print("Closest point is " + closestLurkerPoint.name);
+                gameObject.GetComponent<Transform>().position = closestLurkerPoint.transform.position;
+                currentLurkingPoint = closestLurkerPoint;
+            }
+            else
+            {
+                Debug.LogError("Couldn't find valid point");
+            }
         }
-        
+        else
+        {
+            debugIsVisable = false;
+        }
+    }
 
-
-
+    private bool IsVisableToPlayer(UnityEngine.Vector3 position)
+    {
+        // Player position on the AI camera view
+        UnityEngine.Vector3 screenPoint = playerCam.WorldToViewportPoint(player.GetComponent<Transform>().position);
+        // Is the player within the view bounds
+        bool InScreenBounds = screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+        // Is the player within screen bounds and nothing is obstructing view
+        bool rayObstructed = Physics.Linecast(/*startPos, endPos,*/ position, player.transform.position,
+            out RaycastHit hitinfo, ~((1 << 9) | (1 << 10))); // ignore layer 9 and 10
+                                                              // Print out what the ray hit
+        if (rayObstructed)
+            print("Ray hit: " + hitinfo.collider.name + " at: " + hitinfo.point.x + ", " + hitinfo.point.y);
+        // Debug view
+        return InScreenBounds && !rayObstructed;
     }
 }
--- Assets/3. Scripts/LurkerPoint.cs
+++ Assets/3. Scripts/LurkerPoint.cs
@@ -4,23 +4,15 @@ using UnityEngine;
 
 public class LurkerPoint : MonoBehaviour
 {
-    public bool isVisableByPlayer = false;
+    private Renderer ren;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ren = gameObject.GetComponent<Renderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool IsVisable()
     {
-        if(gameObject.GetComponent<Renderer>().isVisible)
-        {
-            isVisableByPlayer = true;
-        }
-        else
-        {
-            isVisableByPlayer = false;
-        }
+        return ren.isVisible;
     }
 }
