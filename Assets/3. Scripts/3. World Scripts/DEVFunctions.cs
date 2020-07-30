4a3f112e183ddd135f9b00b5319bc49ea493a40e
Connorforce <connorforcedev@gmail.com>
2020-07-30 23:43:48 +1000

    Level Design Update VI.III
    Same as the last 2. Wait for update VII

---
+++ Assets/3. Scripts/3. World Scripts/DEVFunctions.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DEVFunctions : MonoBehaviour
+{
+    public string DevScene = ""; //The scene that will be loaded.
+    public string GameScene = ""; //The scene that will be loaded.
+
+    void Update()
+    {
+
+        //if (Input.GetKey(KeyCode.Tilde)) //Load the dev scene from the main game.
+        if (Input.GetKeyDown(KeyCode.BackQuote))
+        {
+            SceneManager.LoadScene(DevScene);
+            Debug.Log("Developer Room Loaded");
+        }
+
+        if (Input.GetKey(KeyCode.KeypadMultiply)) //Loads the main game.
+        {
+            SceneManager.LoadScene(GameScene);
+            Debug.Log("Welcome to the Dark");
+        }
+
+        if (Input.GetKey(KeyCode.Backspace)) //Quickly kill the game in its tracks.
+        {
+            Application.Quit();
+            Debug.Log("Application Killed");
+        }
+
+        if (Input.GetKey(KeyCode.KeypadPlus))
+        {
+            //SceneManager.LoadScene(SceneName);
+            Debug.Log("Plus Activated");
+        }
+
+        if (Input.GetKey(KeyCode.KeypadMinus))
+        {
+            //SceneManager.LoadScene(SceneName);
+            Debug.Log("Minus Activated");
+        }
+    }
+}
