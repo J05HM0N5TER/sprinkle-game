9ae84b1935405eed10b8197ee946f358915c0397
Sean <seanclading@gmail.com>
2020-08-19 12:22:48 +1000

    added rotation change to lurker and working on notes, nearly done

--- Assets/3. Scripts/Notes.cs
+++ Assets/3. Scripts/Notes.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,17 +8,31 @@ public class Notes : MonoBehaviour
 {
     public string inscription = "";
     [HideInInspector] public bool isActive = false;
-    
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isActive)
+        {
+            player.GetComponentInChildren<TextMeshPro>().text = inscription;
+        }
+        if(!isActive)
+        {
+            player.GetComponentInChildren<TextMeshPro>().text = null;
+        }
     }
-    
+    private void OnTriggerEnter(Collider other)
+    {
+        isActive = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        isActive = false;
+    }
 }
