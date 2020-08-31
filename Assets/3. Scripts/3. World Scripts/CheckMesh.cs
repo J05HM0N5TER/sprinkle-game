b41458bc0bb0c8655de6a14c5a23d5646d160f5f
Connorforce <connorforcedev@gmail.com>
2020-08-31 20:36:41 +1000

    Level Design Update IX.V.II
    Lighting is broken.

---
+++ Assets/3. Scripts/3. World Scripts/CheckMesh.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckMesh : MonoBehaviour
+{
+    public Mesh mesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (mesh != null)
+        {
+            Debug.Log("Vertices: " + mesh.vertexCount);
+            Debug.Log("Triangles: " + mesh.triangles.Length);
+        }
+    }
+}
