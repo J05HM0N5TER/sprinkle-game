8fb4e9fe18a80eefd761565920a7405f5011b181
Sean <seanclading@gmail.com>
2020-08-31 11:03:35 +1000

    working trigger scirpt for animations of doors and such

---
+++ Assets/3. Scripts/TriggerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class TriggerScript : MonoBehaviour
+{
+    public GameObject animatedObject;
+    private Animation refToAnimation;
+    //public AnimationClip[] clip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        refToAnimation = animatedObject.GetComponent<Animation>();
+        //refToAnimation["Open"].layer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        refToAnimation.Play("Open");
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        refToAnimation.Play("Close");
+    }
+}
