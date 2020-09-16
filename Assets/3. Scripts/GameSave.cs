4263ee7211547cd57a856abc03c163a6d66cd8c9
Josh <joshua.francis@live.com.au>
2020-09-16 16:53:35 +1000

    Added test for serialising
    Also added rules to .gitignore to make sure it doesn't upload saves

---
+++ Assets/3. Scripts/GameSave.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System;
+using System.IO;
+
+public class GameSave : MonoBehaviour
+{
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.F1))
+		{
+			Debug.Log("Save button pressed");
+			SaveGame();
+		}
+	}
+
+	public void SaveGame()
+	{
+		XmlDocument xmlDocument = new XmlDocument();
+
+		// Basic file info
+		XmlElement fileInfo = xmlDocument.CreateElement("FileInfo");
+		fileInfo.SetAttribute("FileName", "SaveData");
+		fileInfo.SetAttribute("VersionNumber", "0.01");
+		fileInfo.SetAttribute("Time", DateTime.Now.ToString());
+		fileInfo.SetAttribute("TestID", this.GetInstanceID().ToString());
+
+		xmlDocument.AppendChild(fileInfo);
+
+		XmlAttribute saveData = xmlDocument.CreateAttribute("Save");
+		//saveData.a
+
+		xmlDocument.Save(Application.dataPath + "/Saves/Save.xml");
+		if (File.Exists(Application.dataPath + "/Saves/Save.xml"))
+		{
+			Debug.Log("Game Saved", this);
+		}
+	}
+
+
+	public void LoadGame()
+	{
+
+	}
+}
