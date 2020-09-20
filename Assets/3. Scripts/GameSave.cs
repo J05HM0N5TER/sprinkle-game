fd0b11f4ffb4addb97d0b0570d10ce49013a9d29
Josh <joshua.francis@live.com.au>
2020-09-21 09:22:58 +1000

    Added vscode files and started working on saving
    Added some files to try out vscode with unity, also added some test
    code to check that file IO was working correctly.

--- Assets/3. Scripts/GameSave.cs
+++ Assets/3. Scripts/GameSave.cs
@@ -1,47 +1,51 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using System.Xml;
-using System;
 using System.IO;
+using System.Xml;
+using UnityEngine;
 
 public class GameSave : MonoBehaviour
 {
 
-	private void Update()
+	private void Update ()
 	{
-		if (Input.GetKeyDown(KeyCode.F1))
+		if (Input.GetKeyDown (KeyCode.F1))
 		{
-			Debug.Log("Save button pressed");
-			SaveGame();
+			Debug.Log ("Save button pressed");
+			SaveGame ();
 		}
 	}
 
-	public void SaveGame()
+	public void SaveGame ()
 	{
-		XmlDocument xmlDocument = new XmlDocument();
+		XmlDocument xmlDocument = new XmlDocument ();
 
 		// Basic file info
-		XmlElement fileInfo = xmlDocument.CreateElement("FileInfo");
-		fileInfo.SetAttribute("FileName", "SaveData");
-		fileInfo.SetAttribute("VersionNumber", "0.01");
-		fileInfo.SetAttribute("Time", DateTime.Now.ToString());
-		fileInfo.SetAttribute("TestID", this.GetInstanceID().ToString());
+		XmlElement fileInfo = xmlDocument.CreateElement ("FileInfo");
+		fileInfo.SetAttribute ("FileName", "SaveData");
+		fileInfo.SetAttribute ("VersionNumber", "0.01");
+		fileInfo.SetAttribute ("Time", DateTime.Now.ToString ());
+		fileInfo.SetAttribute ("TestID", this.GetInstanceID ().ToString ());
 
-		xmlDocument.AppendChild(fileInfo);
+		xmlDocument.AppendChild (fileInfo);
 
-		XmlAttribute saveData = xmlDocument.CreateAttribute("Save");
+		XmlAttribute saveData = xmlDocument.CreateAttribute ("Save");
 		//saveData.a
 
-		xmlDocument.Save(Application.dataPath + "/Saves/Save.xml");
-		if (File.Exists(Application.dataPath + "/Saves/Save.xml"))
+		xmlDocument.Save (Application.dataPath + "/Saves/Save.xml");
+		if (File.Exists (Application.dataPath + "/Saves/Save.xml"))
 		{
-			Debug.Log("Game Saved", this);
+			Debug.Log ("Game Saved", this);
 		}
 	}
 
+	private void FixedUpdate ()
+	{
 
-	public void LoadGame()
+	}
+
+	public void LoadGame ()
 	{
 
 	}
