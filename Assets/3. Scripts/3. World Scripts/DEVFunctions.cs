10bbf467bf9224efcc5f34af76fcd40589685fca
Connor Force <connorforcedev@gmail.com>
2020-10-12 17:22:06 +1100

    Game Design Update 4.4.22
    Added final 60s area kit pieces updated model UVs and textures. Added sludge texture to sludge walls.

--- Assets/3. Scripts/3. World Scripts/DEVFunctions.cs
+++ Assets/3. Scripts/3. World Scripts/DEVFunctions.cs
@@ -7,7 +7,7 @@ public class DEVFunctions : MonoBehaviour
     public string DevScene = ""; //The scene that will be loaded.
     public string GameScene = "";
     public string LevelDesignScene = "";
-    public string ProgrammerScene = "";
+    public string Sean3 = "";
 
     void Update()
     {
@@ -39,7 +39,7 @@ public class DEVFunctions : MonoBehaviour
 
         if (Input.GetKey(KeyCode.KeypadMinus))
         {
-            //SceneManager.LoadScene(Sean^3);
+            SceneManager.LoadScene(Sean3);
             Debug.Log("Minus Activated");
         }
     }
