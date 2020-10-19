d5b8cad1bdd854fd97029e15be7360c8273cd3d7
Sean <seanclading@gmail.com>
2020-10-19 15:07:05 +1100

    all suits can now hear sound, and sound for objects more performant

--- Assets/3. Scripts/CollisionNoise.cs
+++ Assets/3. Scripts/CollisionNoise.cs
@@ -22,42 +22,34 @@ public class CollisionNoise : MonoBehaviour
     [Tooltip("Debug, did this thing make noise")]
     public bool madeSound = false; // debug checking if sound was made
     [Tooltip("Debug,timer to remove object from list of places of interest, this is to give time for AI script to detect and use and calculate")]
-    public float timer = 1.0f; // time until the debug is changed and to get rid of the object from list so ai doesnt always get stuck on it forever
-    private GameObject livingSuit; 
     private Rigidbody rb;
-    
+    // All of the suits in the scene
+    private LivingArmourAI[] suits;
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
-        livingSuit = GameObject.Find("LivingArmour");
-        livingSuit.GetComponent<LivingArmourAI>();
+        suits = FindObjectsOfType<LivingArmourAI>();
         rb = GetComponent<Rigidbody>();
         
     }
     /// <summary>
     /// on collision, play a sound and add the object that made the sound to the list of places of interest for the ai to go and walk to.
     /// </summary>
-    /// <param name="collision"></param>
-    /// <returns></returns>
-    private IEnumerator OnCollisionEnter(Collision collision)
+    /// <param name="collision">Object that it collided with</param>
+    private void OnCollisionEnter(Collision collision)
     {
         if(collision.relativeVelocity.magnitude >= minSpeedToMakeSound)
         { 
             audioSource.PlayOneShot(clip);
             madeSound = true;
-            livingSuit.GetComponent<LivingArmourAI>().soundSources.Add(gameObject.transform.position);
-            yield return StartCoroutine("removeFromlist");
+            foreach(var suit in suits)
+            {
+                if(suit.isActiveAndEnabled)
+                {
+                    suit.soundSources.Add(gameObject.transform.position);
+                }
+            }
         }
-    }
-    /// <summary>
-    /// remove the object that made sound for the list so that it is no longer a spot of interest.
-    /// </summary>
-    /// <returns></returns>
-    private IEnumerator removeFromlist()
-    {
-        yield return new WaitForSeconds(timer);
-        madeSound = false;
-        livingSuit.GetComponent<LivingArmourAI>().soundSources.Remove(gameObject.transform.position);
     }
 }
