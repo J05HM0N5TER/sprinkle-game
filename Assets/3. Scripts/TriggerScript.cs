f2853f3a2a0b3311854139ed1eadebf7e8a23dbe
Sean <seanclading@gmail.com>
2020-10-26 15:54:28 +1100

    got the old scripts and added back in as they got lost somehow

--- Assets/3. Scripts/TriggerScript.cs
+++ Assets/3. Scripts/TriggerScript.cs
@@ -27,16 +27,29 @@ public class TriggerScript : MonoBehaviour
     public float timer = 5.0f;
     private float resettimer;
     private bool doorOpen;
+    [HideInInspector] public bool thingInbetweenDoors;
+
+    public Material unlockedmat;
+    public Material lockedmat;
+    public GameObject keypadLeft;
+    public GameObject ledLeft;
+    public GameObject keypadRight;
+    public GameObject ledRight;
     // Start is called before the first frame update
     void Start()
     {
        audioS = soundSource.GetComponent<AudioSource>();
        resettimer = timer;
+       keypadLeft.GetComponent<MeshRenderer>().material = unlockedmat;
+       ledLeft.GetComponent<MeshRenderer>().material = unlockedmat;
+       keypadRight.GetComponent<MeshRenderer>().material = unlockedmat;
+       ledRight.GetComponent<MeshRenderer>().material = unlockedmat;
     }
     private void Update()
     {
+        
         timer -= Time.deltaTime;
-        if (thingsInDoorway.Count == 0 && timer <= 0 && doorOpen)
+        if (thingsInDoorway.Count == 0 && timer <= 0 && doorOpen && !locked && !thingInbetweenDoors)
         {
             animatedObject.GetComponent<Animator>().SetTrigger("Close");
             audioS.PlayOneShot(Closeclip);
@@ -46,43 +59,51 @@ public class TriggerScript : MonoBehaviour
                 hasplayedonce = true;
             }
         }
+        if(thingsInDoorway.Count > 0 && !locked && !doorOpen)
+        {
+            animatedObject.GetComponent<Animator>().SetTrigger("Open");
+            audioS.PlayOneShot(Openclip);
+            timer = resettimer;
+            doorOpen = true;
+            if(oneTimeUse)
+            {
+                hasplayedonce = true;
+            }
+        }
+        if(locked)
+        {
+            keypadLeft.GetComponent<MeshRenderer>().material = lockedmat;
+            ledLeft.GetComponent<MeshRenderer>().material = lockedmat;
+            keypadRight.GetComponent<MeshRenderer>().material = lockedmat;
+            ledRight.GetComponent<MeshRenderer>().material = lockedmat;
+        }
+        else
+        {
+            keypadLeft.GetComponent<MeshRenderer>().material = unlockedmat;
+            ledLeft.GetComponent<MeshRenderer>().material = unlockedmat;
+            keypadRight.GetComponent<MeshRenderer>().material = unlockedmat;
+            ledRight.GetComponent<MeshRenderer>().material = unlockedmat;
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
-        if(!locked)
+        if (!hasplayedonce)
         {
-            if (!hasplayedonce)
+            if(other.tag == "Player" || other.tag == "Enemy")
             {
-                if(other.tag == "Player" || other.tag == "Enemy")
-                {
-                    if(!doorOpen)
-                    {
-                        animatedObject.GetComponent<Animator>().SetTrigger("Open");
-                        audioS.PlayOneShot(Openclip);
-                        timer = resettimer;
-                    }
-                    thingsInDoorway.Add(other.gameObject);
-                    doorOpen = true;
-                    if(oneTimeUse)
-                    {
-                        hasplayedonce = true;
-                    }
-                }
+                thingsInDoorway.Add(other.gameObject);
             }
-        } 
+        }
     }
     private void OnTriggerExit(Collider other)
     {
-        if(!locked)
+        if (!hasplayedonce)
         {
-            if (!hasplayedonce)
+            if (other.tag == "Player" || other.tag == "Enemy")
             {
-                if (other.tag == "Player" || other.tag == "Enemy")
-                {
-                    thingsInDoorway.Remove(other.gameObject);
-                    
-                }     
-            }
-        }  
+                thingsInDoorway.Remove(other.gameObject);
+                
+            }    
+        }
     }
 }
