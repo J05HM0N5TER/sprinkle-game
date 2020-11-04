d9b8d8d943d175587330a77c80aa2899d7ce333d
Connor Force <connorforcedev@gmail.com>
2020-11-05 09:22:18 +1100

    Game Design Update 4.4.63
    Added AIE and Unity logo. Added credits scene. Wrote two new scripts to control the credits. Updated game manager script to include credits scene. Updated DEVFunctions script to include credits scene. Added credits canvas prefab.

---
+++ Assets/3. Scripts/Designer Scripts/CreditsRoll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsRoll : MonoBehaviour
+{
+    public GameObject creditsPanel;
+    public int travelDistance;
+    public float moveSpeed;
+
+    void Update()
+    {
+        Vector2 pos = creditsPanel.transform.position;
+        for (int i = 0; i < travelDistance; i++)
+        {
+            pos.y += moveSpeed * Time.deltaTime;
+            creditsPanel.transform.position = pos;
+        }
+    }
+}
---
+++ Assets/3. Scripts/Designer Scripts/CreditsTimer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+
+public class CreditsTimer : MonoBehaviour
+{
+
+    public int timeLeft = 60; //Seconds overall
+    public Text countdown; //UI countdown timer
+    public string nextScene;
+    public GameObject Thankyou;
+
+    void Start()
+    {
+        StartCoroutine("LoseTime");
+        Time.timeScale = 1; //Setting the correct time scale
+    }
+    void Update()
+    {
+        countdown.text = ("" + timeLeft); //Showing the time on the Canvas
+
+        if (timeLeft <= 0.0f)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(nextScene);
+        }
+
+        if (timeLeft <= 7.0f)
+        {
+            Thankyou.SetActive(true);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(nextScene);
+        }
+
+    }
+    //Coroutine
+    IEnumerator LoseTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+        }
+
+    }
+
+}
