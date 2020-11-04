5bc78d696496461ee27a300fb57f2264df58c108
Sean <seanclading@gmail.com>
2020-11-04 14:39:59 +1100

    reticle changes depending on what is being looked at

--- Assets/3. Scripts/Reticle.cs
+++ Assets/3. Scripts/Reticle.cs
@@ -6,6 +6,9 @@ public class Reticle : MonoBehaviour
 {
     [Tooltip("The object on the canvas that has the reticle sprite")]
     public GameObject reticleSprite;
+    public GameObject lootIcon;
+    public GameObject pressIcon;
+    public GameObject grabIcon;
     private Camera playerCamera;
     [Tooltip("Should be the same as the grabbing distance, just the distance that the ray goes to check whats in front")]
     public float grabDistance = 30;
@@ -23,12 +26,27 @@ public class Reticle : MonoBehaviour
         if(RayOut.collider == null)
         {
             reticleSprite.SetActive(false);
+            lootIcon.SetActive(false);
+            pressIcon.SetActive(false);
+            grabIcon.SetActive(false);
             //Debug.Log("didnt hit shit");
         }
         else//(RayOut.collider.gameObject.layer == LayerMask.NameToLayer("Dynamic"))
         {
             reticleSprite.SetActive(true);
             //Debug.Log("dynamic hit "+ RayOut.transform.gameObject.name);
+        }
+        if(RayOut.collider.tag == "Keyitem")
+        {
+            lootIcon.SetActive(true);
+        }
+        if(RayOut.collider.tag == "interact")
+        {
+            pressIcon.SetActive(true);
+        }
+        if(RayOut.collider.tag == "Dynamic")
+        {
+            grabIcon.SetActive(true);
         }
     }
 }
