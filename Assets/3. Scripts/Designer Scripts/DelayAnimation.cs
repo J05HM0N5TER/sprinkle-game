35c3243eeb8a3b4e568c25f45d31891ad1303eaa
Connor Force <connorforcedev@gmail.com>
2020-11-10 13:48:21 +1100

    Level Design Update 10.0.65
    Added new UI cracks. Added new locker 60s texture. Added new UVed roof meshes. Added new crack variants to the UI Canvas. Added Josh's animation delay script. Added vent animation and set up living suit first appearance area. Added undated 60s Locker textures.

---
+++ Assets/3. Scripts/Designer Scripts/DelayAnimation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayAnimation : MonoBehaviour
+{
+	// Start is called before the first frame update
+	[Tooltip("How long the delay is between triggering it and when the animation plays (in seconds)")]
+	public float delay;
+	[Tooltip("The animator that the state is going to be called on after the delay")]
+	public Animator animator;
+	[Tooltip("The state that the animator is changed to (in the animator graph thing)")]
+	public string animatorPlayName;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			// animator = GetComponent<Animator>();
+			StartCoroutine("DelayAnimationStart");
+		}
+	}
+
+	private IEnumerator DelayAnimationStart()
+	{
+		yield return new WaitForSeconds(delay);
+		animator.Play(animatorPlayName);
+        Destroy(this);
+	}
+
+}
